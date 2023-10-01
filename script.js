48737d016d9fdb549f428c327afd0cc3c429d1b7
mesropaghumyan <mesrop.aghumyan@etu.univ-lyon1.fr>
2023-10-01 23:33:10 +0200

    finish project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+// Imports
 import oc from 'three-orbit-controls';
 
-// Texture management
+// Textures
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 const basicTextureLoader = new THREE.TextureLoader();
 const sunTexture = basicTextureLoader.load('./images/sun.jpeg');
@@ -14,6 +15,8 @@ const uranusTexture = basicTextureLoader.load('./images/uranus.jpg');
 const neptuneTexture = basicTextureLoader.load('./images/neptune.jpg');
 const plutoTexture = basicTextureLoader.load('./images/pluto.jpg');
 const moonTexture = basicTextureLoader.load('./images/moon.jpeg');
+const saturnRingTexture = basicTextureLoader.load('./images/saturn_ring.png');
+const uranusRingTexture = basicTextureLoader.load('./images/uranus_ring.png');
 
 class CelestialObject extends THREE.Object3D {
     material;
@@ -32,30 +35,6 @@ class CelestialObject extends THREE.Object3D {
         this.orbitalPara = orbitalPara;
         this.orbitalCenter = orbitalCenter;
         this.add(this.body);
-
-        if(this.orbitalPara && this.orbitalCenter) {
-            this.ring = new THREE.Mesh(
-                new THREE.RingGeometry(
-                    this.orbitalPara.distance,
-                    this.orbitalPara.distance,
-                    0.03,
-                    128
-                ),
-                new THREE.MeshBasicMaterial(
-                    {
-                        color: 0xffffff,
-                        side: THREE.DoubleSide,
-                        transparent: true,
-                        opacity: 0.2
-                    }
-                )
-            )
-        }
-    }
-    addTo(scene) {
-        if (this.ring) {
-            scene.add(ring);
-        }
     }
 
     // Polar coordinates
@@ -63,36 +42,19 @@ class CelestialObject extends THREE.Object3D {
         this.body.position.x = (
             this.orbitalCenter.body.position.x +
             Math.cos(tic * this.orbitalPara.speed) *
-            this.orbitalPara.distance +
-            this.orbitalPara.eli
+            this.orbitalPara.distance + this.orbitalPara.eli
         );
 
         this.body.position.y = (
             this.orbitalCenter.body.position.y +
             Math.sin(tic * this.orbitalPara.speed) *
-            this.orbitalPara.distance -
-            this.orbitalPara.eli
+            this.orbitalPara.distance - this.orbitalPara.eli
         );
     }
 }
 
-class orbitalPara{
-    x;
-    y;
-    z;
-    speed;
-    distance;
-
-    constructor(x, y, z, speed, distance ){
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.speed = speed;
-        this.distance = distance;
-    }
-}
-
 function initRenderer() {
+
     const renderer = new THREE.WebGLRenderer();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -102,14 +64,50 @@ function initRenderer() {
     return renderer;
 }
 
-function createPlanet(solarSystem, size, texture, orbitalPara, ring, orbitalCenter) {
+function initScene() {
+    const ambiantLight = new THREE.AmbientLight(0x333333);
+    const scene = new THREE.Scene();
+    scene.background = cubeTextureLoader.load([
+        './images/stars.jpg',
+        './images/stars.jpg',
+        './images/stars.jpg',
+        './images/stars.jpg',
+        './images/stars.jpg',
+        './images/stars.jpg'
+    ]);
+    scene.add(ambiantLight);
+
+    const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
+    pointLight.castShadow = true;
+    scene.add(pointLight);
+
+    return scene;
+}
+
+function initCamera() {
+    const camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 5;
+    return camera;
+}
+
+function initOrbitalControl(camera, renderer) {
+    const OrbitControls = oc(THREE);
+    const orbit = new OrbitControls(camera, renderer.domElement);
+    orbit.update();
+
+    return orbit;
+}
+
+function createPlanet(solarSystem, size, texture, orbitalPara, ringTexture, orbitalCenter) {
     let planet = new CelestialObject(
         new THREE.SphereGeometry(size, 128, 128),
         new THREE.MeshStandardMaterial( { map: texture } ),
         {
-            x: orbitalPara.x,
-            y: orbitalPara.y,
-            z: orbitalPara.z,
             eli: orbitalPara.eli,
             speed: orbitalPara.speed,
             distance: orbitalPara.distance
@@ -117,8 +115,20 @@ function createPlanet(solarSystem, size, texture, orbitalPara, ring, orbitalCent
         orbitalCenter
     );
 
+    if(ringTexture) {
+        const ringInnerRadius = 0.9;
+        const ringOuterRadius = 1.5;
+        const ringThetaSegments = 50;
+        const RingGeometry = new THREE.RingGeometry(ringInnerRadius, ringOuterRadius, ringThetaSegments);
+        const RingMaterial = new THREE.MeshBasicMaterial({ map: ringTexture, side: THREE.DoubleSide });
+        const RingMesh = new THREE.Mesh(RingGeometry, RingMaterial);
+        RingMesh.rotation.x = Math.PI / 2;
+        planet.body.add(RingMesh);
+    }
+
     planet.body.castShadow = true;
     planet.body.receiveShadow = true;
+
     solarSystem.add(planet);
 
     return planet;
@@ -126,39 +136,14 @@ function createPlanet(solarSystem, size, texture, orbitalPara, ring, orbitalCent
 
 // Initialisation of the scene / camera / renderer
 const renderer = initRenderer();
-const ambiantLight = new THREE.AmbientLight(0x333333);
-const scene = new THREE.Scene();
-scene.background = cubeTextureLoader.load([
-    './images/stars.jpg',
-    './images/stars.jpg',
-    './images/stars.jpg',
-    './images/stars.jpg',
-    './images/stars.jpg',
-    './images/stars.jpg'
-]);
-scene.add(ambiantLight);
-
-const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
-pointLight.castShadow = true;
-scene.add(pointLight);
-
-const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-camera.position.z = 5;
-
-// Orbit controller
-const OrbitControls = oc(THREE);
-const orbit = new OrbitControls(camera, renderer.domElement);
-orbit.update();
+const scene = initScene();
+const camera = initCamera();
+const orbit = initOrbitalControl(camera, renderer);
 
 // Allowing to calculate the new orbital position of celestial object.
 let tic = 0;
 
-// Initialisation of your objects / materials / light
+// Initialisation objects / materials / light
 let solarSystem = new THREE.Object3D();
 scene.add(solarSystem);
 
@@ -169,16 +154,16 @@ let sun = new CelestialObject(
 );
 solarSystem.add(sun.body);
 
-let mercury = createPlanet(solarSystem, 0.2, mercuryTexture, {x: 1, y: 1, z: 1, eli: 0.205, speed: 0.04, distance: 3}, null, sun);
-let venus = createPlanet(solarSystem, 0.4, venusTexture, {x: 2, y: 2, z: 2, eli: 0.006,speed: -0.015, distance: 6}, null, sun);
-let earth = createPlanet(solarSystem, 0.5, earthTexture, {x: 3, y: 3, z: 3, eli: 0.016,speed: 0.01, distance: 9}, null, sun);
-let moon = createPlanet(solarSystem, 0.1, moonTexture, {x: 0, y: 0, z: 0, eli: 0.0549,speed: 0.05, distance: 1}, null, earth);
-let mars = createPlanet(solarSystem, 0.3, marsTexture,  {x: 4, y: 4, z: 4, eli: 0.093,speed: 0.008, distance: 12}, null, sun);
-let jupiter = createPlanet(solarSystem, 0.9, jupiterTexture, {x: 5, y: 5, z: 5, eli: 0.048,speed: 0.002, distance: 15}, null, sun);
-let saturn = createPlanet(solarSystem, 0.8, saturnTexture, {x: 6, y: 6, z: 6, eli: 0.054,speed: 0.0009, distance: 18}, null, sun);
-let uranus = createPlanet(solarSystem, 0.7, uranusTexture, {x: 7, y: 7, z: 7, eli: 0.047,speed: -0.0004, distance: 21}, null, sun);
-let neptune = createPlanet(solarSystem, 0.3, neptuneTexture, {x: 8, y: 8, z: 8, eli: 0.008,speed: 0.0001, distance: 24}, null, sun);
-let pluto = createPlanet(solarSystem, 0.1, plutoTexture, {x: 9, y: 9, z: 9, eli: 0.002,speed: 0.00007, distance: 27}, null, sun);
+let mercury = createPlanet(solarSystem, 0.2, mercuryTexture, { eli: 0.205, speed: 0.04, distance: 3}, null, sun);
+let venus = createPlanet(solarSystem, 0.4, venusTexture, { eli: 0.006,speed: -0.015, distance: 6}, null, sun);
+let earth = createPlanet(solarSystem, 0.5, earthTexture, {eli: 0.016,speed: 0.01, distance: 9}, null, sun);
+let moon = createPlanet(solarSystem, 0.1, moonTexture, { eli: 0.0549,speed: 0.05, distance: 1}, null, earth);
+let mars = createPlanet(solarSystem, 0.3, marsTexture,  {eli: 0.093,speed: 0.008, distance: 12}, null, sun);
+let jupiter = createPlanet(solarSystem, 0.9, jupiterTexture, { eli: 0.048,speed: 0.002, distance: 15}, null, sun);
+let saturn = createPlanet(solarSystem, 0.8, saturnTexture, { eli: 0.054,speed: 0.0009, distance: 18}, saturnRingTexture, sun);
+let uranus = createPlanet(solarSystem, 0.7, uranusTexture, { eli: 0.047,speed: -0.0004, distance: 21}, uranusRingTexture, sun);
+let neptune = createPlanet(solarSystem, 0.3, neptuneTexture, { eli: 0.008,speed: 0.0001, distance: 24}, null, sun);
+let pluto = createPlanet(solarSystem, 0.1, plutoTexture, { eli: 0.002,speed: 0.00007, distance: 27}, null, sun);
 
 // This is executed for each frames
 function render() {
@@ -227,17 +212,14 @@ function render() {
                 const distance = planete1.body.position.distanceTo(planete2.body.position);
 
                 if (distance < planete1.rayon + planete2.rayon) {
-                    // Appliquer un effet d'ombre ici
-                    // Par exemple, assombrir le matériau de planete2
                     planete2.body.material.color.set(0x555555);
                 } else {
-                    // Réinitialiser le matériau de planete2 si les planètes ne se croisent pas
                     planete2.body.material.color.set(0xFFFFFF);
                 }
             }
         });
     });
 
-    renderer.render(scene, camera); // remplace renderer.render(scene, camera);
+    renderer.render(scene, camera);
 }
 render();
